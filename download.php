423fd2915b97ef4590cdd5727497cf65f3f9a543
Marine <marinepaysafecard@protonmail.com>
2019-03-13 00:18:11 +0100

    Path download flaw

--- download.php
+++ download.php
@@ -2,7 +2,8 @@
 
 if(isset($_GET['dll']) && !empty($_GET['dll']))
 {
-    if(file_exists('upld/' . strip_tags($_GET['dll'])))
+    // preg match is for patch download flaw
+    if(preg_match("#^[a-zA-Z0-9]{50}\/[a-zA-Z0-9._\-+<>:*|?']+\.[a-zA-Z0-9]+$#", $_GET['dll']) && file_exists('upld/' . strip_tags($_GET['dll'])))
     {
         // Here we force download the file to the user using content disposition
 
@@ -10,7 +11,7 @@ if(isset($_GET['dll']) && !empty($_GET['dll']))
 
         header("Content-Description: File Transfer"); 
         header("Content-Type: " . finfo_file($finfo, 'upld/' . $_GET['dll']));
-        header("Content-disposition: attachment; filename=\"" . basename('upld/' . $_GET['dll']) . "\""); 
+        header("Content-disposition: attachment; filename=\"" . basename('upld/' . strip_tags($_GET['dll'])) . "\""); 
         readfile('upld/' . $_GET['dll']); // do the double-download-dance (dirty but worky)
     }
     else { header('Location: index.php?error=Download error : Bad file name or not found.'); }
